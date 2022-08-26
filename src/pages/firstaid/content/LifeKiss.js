110fa2a9675cc37a7a1efb7da7a8e799ed94905d
alinso <webalinso@gmail.com>
2022-08-27 00:09:04 +0300

    dfgh

--- src/pages/firstaid/content/LifeKiss.js
+++ src/pages/firstaid/content/LifeKiss.js
@@ -1,38 +1,38 @@
 export default [
     [
         "1- Hasta/ yaralının sert bir zemine sırt üstü yatırılır ve yanına diz çökülür.\n\ɲ" +
-        "2- Hasta/ yaralının boynunu ve göğsünü saran giysiler açılır\n\ɲ" +
-        "Hasta/ yaralının ağız içi kontrol edilir, görünen yabancı cisim varsa çıkartılır.\n\ɲ" +
-        "Hastaya baş geri çene yukarı pozisyonu verilir ve bak-dinle-hisset yöntemiyle hastanın solunup yapıp yapmadığı 10 sn boyunca kontrol edilir.\n\ɲ" +
-        "Hasta/ yaralının solunumu yok ise önce 112 aranır.\n\ɲ" +
-        "Kalp basısı uygulamak için göğüs kemiğinin alt ve üst ucu tespit edilerek alt yarısına bir elin topuğu yerleştirilir.\n\ɲ" +
-        "Diğer el bu elin üzerine yerleştirilir ve her iki elin parmakları birbirine kenetlenir.\n\ɲ" +
-        "Ellerin parmakları göğüs kafesiyle temas ettirilmeden, dirsek bükülmeden, göğüs kemiği üzerine vücut dik olacak şekilde tutulur.\n\ɲ" +
-        "Göğüs kemiği 4-5 cm aşağı inecek şekilde ( yandan bakıldığında göğüs yüksekliğinin 1/3 ü kadar ) 30 kalp basısı uygulanır. Bu işlemin hızı dakikada 100 bası olacak şekilde ayarlanır.\n\ɲ" +
-        "Baş geri çene yukarı pozisyonu tekrar verilerek hava yolu açıklığı sağlanır.\n\ɲ" +
-        "Alnın üzerine konulan elin baş ve işaret parmağını kullanarak hasta/ yaralının burnu kapatılır\n\ɲ" +
-        "Normal bir soluk alınır, baş geri çene yukarı pozisyonunda iken hasta/ yaralının ağzını içine alacak şekilde ağız yerleştirilir.\n\ɲ" +
-        "Hasta/ yaralının göğsünü yükseltmeye yarayacak kadar her biri 1 saniye süren 2 nefes verilir. Havanın geriye çıkması için zaman verilmelidir.\n\ɲ" +
-        "Hasta/ yaralıya 30 kalp masajından sonra 2 solunum yaptırılır. Bu işlem 5 kez tekrar edildiğinde bir siklusu ifade eder. Her siklusta hasta/ yaralının solunumu ve şah damarından nabzı 5 sn süreyle kontrol edilir.\n\ɲ" +
-        "Temel yaşam desteğine hasta/ yaralının yaşamsal refleksleri veya tıbbi yardım gelene kadar kesintisiz devam edilir.\n\ɲ"
+        "2- Hasta/ yaralının boynunu ve göğsünü saran giysiler açılır \n\ɲ " +
+        "3-Hasta/ yaralının ağız içi kontrol edilir, görünen yabancı cisim varsa çıkartılır.\n\ɲ" +
+        "4-Hastaya baş geri çene yukarı pozisyonu verilir ve bak-dinle-hisset yöntemiyle hastanın solunup yapıp yapmadığı 10 sn boyunca kontrol edilir.\n\ɲ" +
+        "5-Hasta/ yaralının solunumu yok ise önce 112 aranır.\n\ɲ" +
+        "6-Kalp basısı uygulamak için göğüs kemiğinin alt ve üst ucu tespit edilerek alt yarısına bir elin topuğu yerleştirilir.\n\ɲ" +
+        "7-Diğer el bu elin üzerine yerleştirilir ve her iki elin parmakları birbirine kenetlenir.\n\ɲ" +
+        "8-Ellerin parmakları göğüs kafesiyle temas ettirilmeden, dirsek bükülmeden, göğüs kemiği üzerine vücut dik olacak şekilde tutulur.\n\ɲ" +
+        "9-Göğüs kemiği 4-5 cm aşağı inecek şekilde ( yandan bakıldığında göğüs yüksekliğinin 1/3 ü kadar ) 30 kalp basısı uygulanır. Bu işlemin hızı dakikada 100 bası olacak şekilde ayarlanır.\n\ɲ" +
+        "10-Baş geri çene yukarı pozisyonu tekrar verilerek hava yolu açıklığı sağlanır.\n\ɲ" +
+        "11-Alnın üzerine konulan elin baş ve işaret parmağını kullanarak hasta/ yaralının burnu kapatılır\n\ɲ" +
+        "12-Normal bir soluk alınır, baş geri çene yukarı pozisyonunda iken hasta/ yaralının ağzını içine alacak şekilde ağız yerleştirilir.\n\ɲ" +
+        "13-Hasta/ yaralının göğsünü yükseltmeye yarayacak kadar her biri 1 saniye süren 2 nefes verilir. Havanın geriye çıkması için zaman verilmelidir.\n\ɲ" +
+        "14-Hasta/ yaralıya 30 kalp masajından sonra 2 solunum yaptırılır. Bu işlem 5 kez tekrar edildiğinde bir siklusu ifade eder. Her siklusta hasta/ yaralının solunumu ve şah damarından nabzı 5 sn süreyle kontrol edilir.\n\ɲ" +
+        "15-Temel yaşam desteğine hasta/ yaralının yaşamsal refleksleri veya tıbbi yardım gelene kadar kesintisiz devam edilir.\n\ɲ"
     ],
     [],
     [
-        "O paciente / ferido é colocado de costas sobre uma superfície dura e ajoelha-se ao seu lado. \n\ɲ" +
-        "As roupas que cobrem o pescoço e o peito do paciente/doente lesionado são desapertadas. \n\ɲ" +
-        "A boca do paciente/lesionado é verificada, e qualquer corpo estranho visível é removido. \n\ɲ" +
-        "O paciente é colocado numa posição de cabeça para cima e a respiração do paciente é verificada durante 10 segundos usando o método \"ver-ouvir-sentir-sentir\". \n\ɲ" +
-        "Se o paciente/ferido não tiver respiração, deve-se chamar em primeiro lugar o serviço de ambulância de emergência. \n\ɲ" +
-        "Para aplicar a compressão cardíaca, são identificadas as extremidades inferior e superior do esterno e o pulso uma das mãos é colocado na metade inferior. \n\ɲ" +
-        "A outra mão é colocada sobre esta e os dedos de ambas estão entrelaçados. \n\ɲ" +
-        "Os dedos das mãos são mantidos esticados no esterno, sem tocar no esterno e sem dobrar o cotovelo. \n\ɲ" +
-        "São aplicadas 30 compressões cardíacas para que o esterno desça 4-5 cm (1/3 da altura do peito quando visto de lado). A velocidade deste processo é definida em 100 pressões por minuto. \n\ɲ" +
-        "A posição de queixo para cima é restabelecida e as vias respiratórias são abertas. \n\ɲ" +
-        "Usando o polegar e o indicador da mão colocados na testa, o nariz do paciente é fechado. \n\ɲ" +
-        "A respiração normal é feita, enquanto a cabeça está na posição queixo para cima, a boca é colocada para incluir a boca do paciente.\n\ɲ" +
-        "Dar 2 respirações, cada uma com a duração de 1 segundo, o suficiente para levantar o peito do paciente. Dê tempo para o ar recuar. \n\ɲ" +
-        "Após 30 massagens cardíacas, o paciente/lesionado é submetido a 2 respirações. Quando este processo é repetido 5 vezes, ele representa um ciclo. Em cada ciclo, a respiração do paciente/lesionado e o pulso carotídeo são verificados durante 5 segundos. \n\ɲ" +
-        "O suporte básico de vida é continuado sem interrupções até que os reflexos vitais do paciente/lesionado ou a ajuda médica cheguem."
+        "1-O paciente / ferido é colocado de costas sobre uma superfície dura e ajoelha-se ao seu lado. \n\ɲ" +
+        "2-As roupas que cobrem o pescoço e o peito do paciente/doente lesionado são desapertadas. \n\ɲ" +
+        "3-A boca do paciente/lesionado é verificada, e qualquer corpo estranho visível é removido. \n\ɲ" +
+        "4-O paciente é colocado numa posição de cabeça para cima e a respiração do paciente é verificada durante 10 segundos usando o método \"ver-ouvir-sentir-sentir\". \n\ɲ" +
+        "5-Se o paciente/ferido não tiver respiração, deve-se chamar em primeiro lugar o serviço de ambulância de emergência. \n\ɲ" +
+        "6-Para aplicar a compressão cardíaca, são identificadas as extremidades inferior e superior do esterno e o pulso uma das mãos é colocado na metade inferior. \n\ɲ" +
+        "7-A outra mão é colocada sobre esta e os dedos de ambas estão entrelaçados. \n\ɲ" +
+        "8-Os dedos das mãos são mantidos esticados no esterno, sem tocar no esterno e sem dobrar o cotovelo. \n\ɲ" +
+        "9-São aplicadas 30 compressões cardíacas para que o esterno desça 4-5 cm (1/3 da altura do peito quando visto de lado). A velocidade deste processo é definida em 100 pressões por minuto. \n\ɲ" +
+        "10-A posição de queixo para cima é restabelecida e as vias respiratórias são abertas. \n\ɲ" +
+        "11-Usando o polegar e o indicador da mão colocados na testa, o nariz do paciente é fechado. \n\ɲ" +
+        "12-A respiração normal é feita, enquanto a cabeça está na posição queixo para cima, a boca é colocada para incluir a boca do paciente.\n\ɲ" +
+        "13-Dar 2 respirações, cada uma com a duração de 1 segundo, o suficiente para levantar o peito do paciente. Dê tempo para o ar recuar. \n\ɲ" +
+        "14-Após 30 massagens cardíacas, o paciente/lesionado é submetido a 2 respirações. Quando este processo é repetido 5 vezes, ele representa um ciclo. Em cada ciclo, a respiração do paciente/lesionado e o pulso carotídeo são verificados durante 5 segundos. \n\ɲ" +
+        "15O suporte básico de vida é continuado sem interrupções até que os reflexos vitais do paciente/lesionado ou a ajuda médica cheguem."
 
     ]
 ]
