4efe2613cb87e82b492f632157811b53176b2831
alinso <webalinso@gmail.com>
2022-08-26 23:25:12 +0300

    dfgh

--- src/pages/firstaid/content/BasicApplications.js
+++ src/pages/firstaid/content/BasicApplications.js
@@ -1,15 +1,18 @@
 export default[
     [
-        "Koruma, Bildirme ve Kurtarma olarak ifade edilir. " +
+    "Koruma, Bildirme ve Kurtarma olarak ifade edilir. " +
     " İlk yardımcı, acil bir durumda yaşam zincirinin birinci halkasında yer alan üç temel uygulama olan koruma, bildirme ve kurtarmayı öncelikli olarak yapmalıdır.  ",
 
-    "Koruma: Kaza sonuçlarının ağırlaşmasını önlemek için olay yerinde değerlendirme yapmak. " +
+    "<b>Koruma</b>\n\n" +
+     " Kaza sonuçlarının ağırlaşmasını önlemek için olay yerinde değerlendirme yapmak. " +
     "Olay yerinde oluşabilecek tehlikeleri belirleyerek güvenli bir çevre oluşturmak. ",
 
-    "Bildirme: Olay mümkün olduğunca hızlı birşekilde Acil Sağlık Hizmetleri birimine bildirilir. Bildirim yapılırken son derece sakin olunmalı veya sakin birisinin araması sağlanmalı, sorulan " +
+    "<b>Bildirme</b>\n\n" +
+    "Olay mümkün olduğunca hızlı bir şekilde Acil Sağlık Hizmetleri birimine bildirilir. Bildirim yapılırken son derece sakin olunmalı veya sakin birisinin araması sağlanmalı, sorulan " +
     "sorulara net cevap verilmeli, adres bilgisi verilirken cadde sokak isimleri net verilmeli ve yakınlarda herkes tarafından kolaylıkla bilinecek yerler tarif edilmelidir ",
 
-    "Kurtarma: Olay yerinde yaralılara müdahale yapılırken hızlı ancak sakin olmalıdır. İlk yardımcı önce kendi güvenliğini sonra olay yeri güvenliğini sağlamalıdır. Daha sonra hasta veya yaralının\t birinci değerlendirilmesi yapılır.\t " +
+    "<b>Kurtarma</b>\n\n" +
+    " Olay yerinde yaralılara müdahale yapılırken hızlı ancak sakin olmalıdır. İlk yardımcı önce kendi güvenliğini sonra olay yeri güvenliğini sağlamalıdır. Daha sonra hasta veya yaralının birinci değerlendirilmesi yapılır.  " +
     "Birinci değerlendirmede amaç;Hastaveyayaralıiçinhayatitehlikeoluşturanunsurları belirlemek ve zamanında müdahale etmektir. Öncelikle bilinç değerlendirmesi yapılır çünkü bilincin kaybolmasından sonra çok kısa bir süre içinde solunum ve kalp durması gelişebilir. " +
     "Bilinç kontrolündeyetişkin ve çocuk hasta veya yaralının omzuna dokunarak “iyi misiniz?” diye sorulur. Bebeklerde ise ayak tabanına hafifçe dokunularak bilinç kontrolü yapılır. Cevap alınabiliyorsa bilinç var demektir ve ikinci değerlendirmeye geçilir. Cevap alınamıyorsa bilinç yok anlamına gelir ve bildirim yapılarak acil tıbbiyardım istenir. Daha sonra hasta veya yaralının, ABC değerlendirmesine geçilir. ",
     ]
@@ -19,14 +22,15 @@ export default[
         "Existem 3 aplicações básicas chamadas Proteção, Notificação e Recuperação.  " +
         "Em caso de emergência, o primeiro socorrista deve dar prioridade à protecção, à comunicação e ao salvamento, que são as três práticas básicas na primeira etapa da cadeia de vida.  ",
 
-        "Proteção: Fazer uma avaliação no local para evitar o agravamento das consequências do acidente. Estabelecer um ambiente seguro, identificando os perigos que podem ocorrer neste. ",
+        "<b>Proteção</b>\n\n" +
+        " Fazer uma avaliação no local para evitar o agravamento das consequências do acidente. Estabelecer um ambiente seguro, identificando os perigos que podem ocorrer neste. ",
 
-        "Notificação: O incidente é relatado à unidade de Serviços de Saúde de Emergência o mais rapidamente possível. Ao fazer a notificação, deve ser mantida a calma ou uma pessoa calma deve ser chamada, perguntas devem ser respondidas de forma clara, nomes de ruas devem ser dados claramente ao dar informações sobre moradas, e lugares que possam ser facilmente conhecidos por todos devem ser descritos.  ",
+        "<b>Notificação</b>\n\n" +
+        " O incidente é relatado à unidade de Serviços de Saúde de Emergência o mais rapidamente possível. Ao fazer a notificação, deve ser mantida a calma ou uma pessoa calma deve ser chamada, perguntas devem ser respondidas de forma clara, nomes de ruas devem ser dados claramente ao dar informações sobre moradas, e lugares que possam ser facilmente conhecidos por todos devem ser descritos.  ",
 
-        "Recuperação: As pessoas que respondem aos feridos no local devem ser rápidas, mas calmas. O primeiro socorrista deve primeiro garantir a sua própria segurança e depois a segurança do local de incidente. Depois, é feita a primeira avaliação do paciente ou do ferido.  " +
-        " " +
+        "<b>Recuperação</b>\n\n" +
+        " As pessoas que respondem aos feridos no local devem ser rápidas, mas calmas. O primeiro socorrista deve primeiro garantir a sua própria segurança e depois a segurança do local de incidente. Depois, é feita a primeira avaliação do paciente ou do ferido.  " +
         "Na primeira avaliação, o objetivo é identificar os fatores de risco de vida para o paciente ou ferido e intervir de forma oportuna. Em primeiro lugar, a consciência é avaliada porque a paragem respiratória e cardíaca pode desenvolver-se num período de tempo muito curto após a perda da consciência.  " +
-        " " +
         "Durante a verificação de consciência, o paciente é questionado sobre o seu estado ao tocar no ombro da pessoa ferida. Em bebés, a consciência é verificada tocando levemente na planta dos pés. Se for possível obter uma resposta, isso significa que há consciência e a segunda avaliação é iniciada. Se não houver resposta, a ajuda médica de emergência é solicitada por notificação. Em seguida, é iniciada a avaliação ABC do paciente ou da pessoa ferida. "
 
 
