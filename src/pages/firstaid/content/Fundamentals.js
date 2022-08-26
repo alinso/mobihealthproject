4b4c5226a99cdd419a3a8d4bef2564b9571aa5b1
alinso <webalinso@gmail.com>
2022-08-26 23:00:01 +0300

    dfgh

--- src/pages/firstaid/content/Fundamentals.js
+++ src/pages/firstaid/content/Fundamentals.js
@@ -1,7 +1,16 @@
+
+
+
+"gfdhhfj","dfghj"+
+" dfghjk"
+
+
+
 export default [
     [
         "<b>İlk yardım</b>\n\n" +
         "Ani olarak ortaya çıkan hastalık veya yaralanma durumunda; kişinin hayatını korumak, sağlık durumunun kötüleşmesini önlemek ve iyileşmesine destek olmak amacıyla olay yerindeki mevcut imkânlarla yapılan hızlı ve etkili müdahalelerdir.İlk yardım uygulamaları profesyonel tıbbi yardım sağlanıncaya kadar geçici desteği ifade eder ve asıl tıbbi tanı ve tedavi yöntemlerinin yerine geçmez.",
+
 
         "<b>Acil Tedavi</b>\n\n  " +
         "Acil tedavi ünitelerinde hasta/ yaralılara doktor ve sağlık personeli tarafından yapılan tıbbi müdahalelerdir. ",
@@ -16,52 +25,43 @@ export default [
         "Hasta/ yaralının en yakın sağlık kuruluşuna güvenle ulaşımının sağlanmasına yardımcı olmaktır. ",
 
 
-        "Yaşam Kurtarma Zinciri " +
+        "<b>Yaşam Kurtarma Zinciri </b>\n\n " +
         "Olaya ilk tanık olan kişiden başlayarak tedavinin tamamlandığı hastane ortamına kadar kesintisiz olarak sürdürülen ilk yardım ve acil bakım uygulamalarının tümü “ Yaşam Kurtarma Zinciri“ olarak adlandırılır. " +
-        " " +
+        " \n\n" +
         "Yaşam kurtarma zinciri 4 halkadan oluşur. İlk iki halkası ilkyardımcının sorumluluğunda bulunan ve temel ilk yardım uygulamalarını kapsayan; koruma, bildirme, kurtarma ve olay yerinde sağlanan temel yaşam desteğidir. Son iki halkası ise ilkyardımcının görevi olmayıp profesyonel sağlık ekiplerinin görevidir. " +
         " " +
-        "HALKA " +
-        "Sağlık kuruluşuna haber verilmesi " +
-        "HALKA " +
-        "Olay yerinde Temel Yaşam Desteğiverilmesi " +
-        "HALKA " +
-        "Ambulans ekipleri tarafından müdahale yapılması " +
-        "HALKA " +
-        "Hastane acil servislerinde müdahale yapılması " +
-        " "
+        "1.HALKA:  Sağlık kuruluşuna haber verilmesi " +
+        "2.HALKA : Olay yerinde Temel Yaşam Desteği verilmesi " +
+        "3.HALKA : Ambulans ekipleri tarafından müdahale yapılması " +
+        "4.HALKA : Hastane acil servislerinde müdahale yapılması "
+
 
     ],
     [],
     [
-        "Primeiros Socorros " +
+        "<b>Primeiros Socorros<b>\n\n " +
         "Em caso de lesão ou doença súbita, são intervenções rápidas e eficazes feitas com os meios disponíveis no local, para proteger a vida da pessoa, prevenir a deterioração do estado de saúde e apoiar na sua recuperação. O uso de primeiros socorros representa um apoio temporário até que seja prestada assistência médica profissional e não substitui o diagnóstico médico e os métodos de tratamento propriamente ditos. ",
 
-        "Tratamento de Emergência  " +
+        "<b>Tratamento de Emergência<b>\n\n  " +
         "São as intervenções médicas feitas por médicos e profissionais de saúde aos doentes/lesionados em unidades de tratamento. ",
 
-        "Suporte Básico de Vida  " +
+        "<b>Suporte Básico de Vida<b>\n\n  " +
         "Estas são as intervenções manuais que são realizadas para fornecer oxigénio aos pulmões com respiração artificial e para bombear sangue do coração com massagem cardíaca externa, após a permeabilidade das vias aéreas ser fornecida com o propósito de salvar vidas. ",
 
-        "Objetivos dos Primeiros Socorros " +
+        "<b>Objetivos dos Primeiros Socorros<b>\n\n " +
         "1- Garantir a manutenção das funções vitais,  " +
         "2- Evitar o agravamento do estado dos doentes ou feridos,  " +
         "3- Facilitar a recuperação,  " +
         "4- Ajudar o paciente a chegar com segurança à instituição de saúde mais próxima. ",
 
-        "Cadeia salva-vidas  " +
-        "Todas as práticas de primeiros socorros e cuidados de emergência, que continuam ininterruptamente, desde a pessoa que testemunhou o evento pela primeira vez, até o ambiente hospitalar onde o tratamento é concluído, são chamadas de \"Cadeia de Salva-Vidas\". " +
-        " " +
+        "<b>Cadeia salva-vidas<b>\n\n  " +
+        "Todas as práticas de primeiros socorros e cuidados de emergência, que continuam ininterruptamente, desde a pessoa que testemunhou o evento pela primeira vez, até o ambiente hospitalar onde o tratamento é concluído, são chamadas de Cadeia de Salva-Vidas " +
         "A cadeia salva-vidas consiste em 4 passos. As duas primeiras etapas são proteção, relatório, resgate e suporte básico de vida fornecido no local, que é de responsabilidade do primeiro socorrista e cobre as práticas básicas de primeiros socorros. Os dois últimos passos não são o dever do socorrista, mas o dever das equipas de profissionais de saúde. " +
-        "PASSO " +
-        "Notificar a instituição de saúde " +
-        "PASSO " +
-        "Fornecer suporte básico de vida no local " +
-        "PASSO " +
-        "Intervenção de equipas de ambulância " +
-        "PASSO " +
-        "Intervenção em serviços de emergência hospitalar " +
-        " "
+        "1.PASSO : Notificar a instituição de saúde " +
+        "2.PASSO : Fornecer suporte básico de vida no local " +
+        "3.PASSO:  Intervenção de equipas de ambulância " +
+        "4.PASSO :Intervenção em serviços de emergência hospitalar "
+
     ],
     []
 ];
